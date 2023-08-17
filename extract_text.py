1c2adcd9379f5138008016374a0218d30a25ce00
jmanc <joem@brown.edu>
2023-08-17 15:51:54 -0400

    Add files via upload

---
+++ extract_text.py
@@ -0,0 +1,13 @@
+import fitz  # PyMuPDF
+
+# Open the PDF
+pdf_document = fitz.open('/path/to/pdf/filename.pdf')
+
+# Loop through pages and extract text
+for page_num in range(pdf_document.page_count):
+    page = pdf_document.load_page(page_num)
+    text = page.get_text("text")
+    print(text)
+
+# Close the PDF
+pdf_document.close()
