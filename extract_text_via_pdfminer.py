684363e4655f62543ff447499660e7cff656e903
Birkin James Diana <birkin.diana@gmail.com>
2023-08-18 14:14:37 -0400

    incorporates pdfminer into python-script.

--- extract_text_via_pdfminer.py
+++ extract_text_via_pdfminer.py
@@ -1,4 +1,12 @@
 #https://pdfminersix.readthedocs.io/en/latest/
 
-pip install pdfminer.six
-pdf2txt.py -o output.txt ./testdfg8m6r6.pdf
+# pip install pdfminer.six
+# pdf2txt.py -o output.txt ./testdfg8m6r6.pdf
+
+import pdfminer
+
+from pdfminer.high_level import extract_text
+text = extract_text( './testdfg8m6r6.pdf' )
+print(repr(text))
+'Hello \n\nWorld\n\nHello \n\nWorld\n\nH e l l o  \n\nW o r l d\n\nH e l l o  \n\nW o r l d\n\n\x0c'
+print(text)
